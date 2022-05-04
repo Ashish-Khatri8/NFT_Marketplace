588fc7f8761d017c933a76e7d90412a3d8f5515b
Ashish Khatri <khatri8.ashish@gmail.com>
2022-05-04 13:04:44 +0530

    Included deployment of MarketPlace contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,26 +1,35 @@
 const { ethers } = require("hardhat");
 
 async function main() {
+    // Deploy the ERC20 token contract
     const BlazeToken = await ethers.getContractFactory("BlazeToken");
-    const NFT = await ethers.getContractFactory("NFT");
-
-    // Deploy ERC20 token contract.
     const blazeToken = await BlazeToken.deploy(
-        1000000,
+        100000000,
         "BlazeK",
         "BLZ"
     );
     await blazeToken.deployed();
+    console.log("\nBlazeToken contract deployed at: ", blazeToken.address);
 
-    // Deploy ERC721 token contract.
+    // Deploy the NFT token contract
+    const NFT = await ethers.getContractFactory("NFT");
     const nft = await NFT.deploy(
         "NFT_Token",
         "NFT"
     );
     await nft.deployed();
+    console.log("\nNFT contract deployed at: ", nft.address);
 
-    console.log("BlazeToken is deployed at: ", blazeToken.address);
-    console.log("NFT deployed at: ", nft.address);
+    // Deploy the Marketplace contract
+    const Marketplace = await ethers.getContractFactory("MarketPlace");
+    const marketplace = await Marketplace.deploy(
+        blazeToken.address,
+        nft.address
+    );
+    await marketplace.deployed();
+
+    // Print contract addresses.
+    console.log("\nMarketPlace contract deployed at: ", marketplace.address);
 }
 
 main()
